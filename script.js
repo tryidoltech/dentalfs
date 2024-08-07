606ac53a9a3fcfc2ad7812309fae0b982b8dc734
dps-broken <107934635+dps-broken@users.noreply.github.com>
2024-08-07 11:51:05 +0530

    7 august

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ menuIcon.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 
+
+// -------------------code for services section slider ---------------------------------
+
 let currentIndex = 0;
 const carousel = document.querySelector('.carousel');
 const cards = document.querySelectorAll('.card');
@@ -29,3 +32,39 @@ function prevSlide() {
 
 setInterval(nextSlide, 2000);
 
+
+// --------------code for before after slider-------------------------------
+
+
+
+const sliderButton = document.querySelector('.slider-button');
+const imageAfter = document.querySelector('.image-after');
+const sliderContainer = document.querySelector('.slider-container');
+
+sliderButton.addEventListener('mousedown', startDragging);
+
+function startDragging(e) {
+    document.addEventListener('mousemove', drag);
+    document.addEventListener('mouseup', stopDragging);
+}
+
+function drag(e) {
+    const containerRect = sliderContainer.getBoundingClientRect();
+    let offsetX = e.clientX - containerRect.left;
+    
+    if (offsetX < 0) {
+        offsetX = 0;
+    } else if (offsetX > containerRect.width) {
+        offsetX = containerRect.width;
+    }
+    
+    const percentage = (offsetX / containerRect.width) * 100;
+    
+    imageAfter.style.clipPath = `inset(0 ${100 - percentage}% 0 0)`;
+    sliderButton.style.left = `${percentage}%`;
+}
+
+function stopDragging() {
+    document.removeEventListener('mousemove', drag);
+    document.removeEventListener('mouseup', stopDragging);
+}
