4a5873be937a139278100e40c8335c9a37e4eace
dps-broken <107934635+dps-broken@users.noreply.github.com>
2024-08-07 13:51:08 +0530

    up

--- script.js
+++ script.js
@@ -38,30 +38,39 @@ showCard(currentIndex);
 // --------------code for before after slider-------------------------------
 
 
-
 const sliderButton = document.querySelector('.slider-button');
 const imageAfter = document.querySelector('.image-after');
-const sliderContainer = document.querySelector('.slider-container');
+const sliderContainer = document.querySelector('.slider-container2');
 
 sliderButton.addEventListener('mousedown', startDragging);
+sliderButton.addEventListener('touchstart', startDragging);
 
 function startDragging(e) {
+    e.preventDefault();
     document.addEventListener('mousemove', drag);
     document.addEventListener('mouseup', stopDragging);
+    document.addEventListener('touchmove', drag);
+    document.addEventListener('touchend', stopDragging);
 }
 
 function drag(e) {
+    e.preventDefault();
     const containerRect = sliderContainer.getBoundingClientRect();
-    let offsetX = e.clientX - containerRect.left;
-    
+    let offsetX;
+    if (e.type === 'touchmove') {
+        offsetX = e.touches[0].clientX - containerRect.left;
+    } else {
+        offsetX = e.clientX - containerRect.left;
+    }
+
     if (offsetX < 0) {
         offsetX = 0;
     } else if (offsetX > containerRect.width) {
         offsetX = containerRect.width;
     }
-    
+
     const percentage = (offsetX / containerRect.width) * 100;
-    
+
     imageAfter.style.clipPath = `inset(0 ${100 - percentage}% 0 0)`;
     sliderButton.style.left = `${percentage}%`;
 }
@@ -69,4 +78,6 @@ function drag(e) {
 function stopDragging() {
     document.removeEventListener('mousemove', drag);
     document.removeEventListener('mouseup', stopDragging);
+    document.removeEventListener('touchmove', drag);
+    document.removeEventListener('touchend', stopDragging);
 }
